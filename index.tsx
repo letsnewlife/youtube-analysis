3c68c6731ceefbb3fdf2d54b2e4beed3a54a3b0e
letsnewlife <pjsnew4419@gmail.com>
2025-12-21 01:55:54 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,18 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+const root = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(root).render(
+<React.StrictMode>
+<Auth0Provider
+clientId={import.meta.env.AUTH0_CLIENT_ID}
+domain={import.meta.env.AUTH0_DOMAIN}
+authorizationParams={{
+redirect_uri: window.location.origin
+}}
+>
+<App />
+</Auth0Provider>
+</React.StrictMode>
 );
